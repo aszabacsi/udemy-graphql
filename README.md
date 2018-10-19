66e99291438678efc7cc29227bf672ee01d4528d
Akos Szabacsi <akos.szabacsi@gmail.com>
2018-10-19 23:13:59 +0200

    resolved circular dependencies

---
+++ README.md
@@ -0,0 +1,5 @@
+# GraphQL
+
+## Circular dependencies
+
+If fields object create circular dependencies one must define them as functions
--- types/index.js
+++ types/index.js
@@ -1,5 +1,59 @@
-const CompanyType = require('./company');
-const UserType = require('./user');
+const axios = require('axios');
+const graphql = require('graphql');
+
+const {
+  GraphQLInt,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+} = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: (parentValue, args) => {   // Custom types require resolve functions
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}/users`) // Referring to parentValue 
+        .then(resp => resp.data);
+      } 
+    }
+  })
+});
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    lastName: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    },
+    company: {
+      type: CompanyType,   // Custom type
+      resolve: (parentValue, args) => {   // Custom types require resolve functions
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`) // Referring to parentValue 
+        .then(resp => resp.data);
+      }
+    }
+  })
+});
 
 module.exports = {
   Company: CompanyType,
