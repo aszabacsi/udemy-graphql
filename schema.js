cdd345d7fa29a91ad63e45e21e8d9400dbf7183d
Akos Szabacsi <akos.szabacsi@gmail.com>
2018-10-19 20:38:13 +0200

    using custom types, nested queries

--- schema.js
+++ schema.js
@@ -1,36 +1,18 @@
 const graphql = require('graphql');
 const axios = require('axios');
+const types = require('./customTypes')
 
 const {
-  GraphQLInt,
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema
 } = graphql;
 
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: {
-      type: GraphQLString,
-    },
-    firstName: {
-      type: GraphQLString
-    },
-    lastName: {
-      type: GraphQLString
-    },
-    age: {
-      type: GraphQLInt
-    }
-  }
-});
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
-      type: UserType,
+      type: types.User,
       args: {
         id: {
           type: GraphQLString
